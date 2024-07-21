f5aa245a014a2a46e09fd0b3e983a9ceb08ecf53
EC2 Default User <ec2-user@ip-172-31-38-151.ec2.internal>
2024-07-21 06:53:44 +0000

    Initial commit

---
+++ test/s3_bucket_project.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as S3BucketProject from '../lib/s3_bucket_project-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/s3_bucket_project-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new S3BucketProject.S3BucketProjectStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
